df9bb54c532196406e75e54ebdf8621a139e484d
perf2k2 <perf2k2@gmail.com>
2017-03-08 23:55:23 +0300

    Fix tests

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Facades for standard Yii2 application components
-[![Build Status](https://travis-ci.org/perf2k2/yii2-sugar.svg?branch=master)](https://travis-ci.org/perf2k2/yii2-sugar) [![Coverage Status](https://coveralls.io/repos/github/perf2k2/yii2-sugar/badge.svg?branch=master)](https://coveralls.io/github/perf2k2/yii2-sugar?branch=master)
+[![Build Status](https://travis-ci.org/perf2k2/yii2-facades.svg?branch=master)](https://travis-ci.org/perf2k2/yii2-facades) [![Coverage Status](https://coveralls.io/repos/github/perf2k2/yii2-sugar/badge.svg?branch=master)](https://coveralls.io/github/perf2k2/yii2-sugar?branch=master)
 
 ## Why? 
 In order to install cookie code was not so
@@ -11,7 +11,7 @@ $cookies->add(new \yii\web\Cookie([
     'value' => 'zh-CN',
 ]));
 ```
-
+ 
 and so
 
 ```php
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,29 @@
 <?php
+define('YII_ENV', 'test');
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 
-defined('YII_DEBUG') or define('YII_DEBUG', false);
-defined('YII_ENV') or define('YII_ENV', 'prod');
+require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require __DIR__ .'/../vendor/autoload.php';
 
-require(__DIR__ . '/app/vendor/autoload.php');
-require(__DIR__ . '/app/vendor/yiisoft/yii2/Yii.php');
-
-$config = require(__DIR__ . '/app/config/web.php');
-$config['components']['assetManager']['basePath'] =  '@app/web/assets';
-
-(new yii\web\Application($config))->run();
+(new yii\web\Application([
+    'id' => 'basic',
+    'basePath' => __DIR__ . '/app/',
+    'bootstrap' => ['log'],
+    'components' => [
+        'assetManager' => [
+            'basePath' => '@app/web/assets',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+        ],
+        'request' => [
+            'cookieValidationKey' => 'VOvDMw_R5PFcTrbh6r4P6cXt0yQE3qmb',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+]))->run();
 
 spl_autoload_register(function($class) {
     if (strpos($class, 'perf2k2') !== false) {
