4e83f0d7f9093ceb82470001dfd13fdf75aab015
perf2k2 <perf2k2@gmail.com>
2017-03-19 00:24:48 +0300

    Bugfix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,8 @@
 define('YII_ENV', 'test');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
-require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
-require __DIR__ .'/../vendor/autoload.php';
+require_once(__DIR__ . '/app/vendor/yiisoft/yii2/Yii.php');
+require __DIR__ .'/app/vendor/autoload.php';
 
 (new yii\web\Application([
     'id' => 'basic',
